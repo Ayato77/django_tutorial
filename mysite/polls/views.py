3c6dce35b4f972f2ceb11034bafb2960f8cebda6
Otoya <otoya.nakakaze@gmail.com>
2020-03-16 12:29:18 +0100

    Create simple view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpReponse
+
+# 簡単なビュー作成。ビューを呼び出すためにはURLを対応づける必要がある -> URLconf
+# アプリケーションのディレクトリにurls.pyを作成して、URLconfを作る。
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
