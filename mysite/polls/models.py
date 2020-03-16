86eaf12dc9bc9580efe9f09cc763e68ffae9e018
Otoya <otoya.nakakaze@gmail.com>
2020-03-16 22:39:30 +0100

    Update models.py
    __str__メソッドを各モデルに追加。さらにwas_puclished_recentlyメソッドをQuestionモデルに追加

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create 2 models:
 # 1. Poll with 2 columns question and publication date
@@ -11,11 +14,20 @@ from django.db import models
 # 2. これらの変更のためのマイグレーションを作成するために python manage.py makemigrations を実行します。
 # 3. データベースにこれらの変更を適用するために python manage.py migrate を実行します。
 
+# 各モデルに__str()__メソッドを追加するように心がけること！！
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)# CharFieldは引数としてmax_legth()が必須
     pub_date = models.DateTimeField('date published')# pub_dateでは人間に読めないので、新たにデータ名としてdate publishedを設定
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)# それぞれのchoiceが一つのquestionと関連づけられている
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
