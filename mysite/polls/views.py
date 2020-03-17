70cf7dba30ae278b73b64ae7a651cc7799f2a7a2
Otoya <otoya.nakakaze@gmail.com>
2020-03-17 11:24:05 +0100

    Update view.py
    templateのロードの代わりにrenderを使用

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
-from django.template import loader
 from .models import Question
 
 '''
@@ -20,11 +18,10 @@ from .models import Question
 def index(request):
     # 最新５つの質問をレスポンスする
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # テンプレートのロード
-    template = loader.get_template('polls/index.html')
     # コンテキストはテンプレート変数名をPythonオブジェクトにマッピングする辞書
     context = {'latest_question_list':latest_question_list}
-    return HttpResponse(template.render(context, request))
+    # renderをつかうとテンプレートのロードをわざわざしなくてもいい
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
