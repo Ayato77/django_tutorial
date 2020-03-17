aedd391395133882c394d8622281a530a2a625a8
Otoya <otoya.nakakaze@gmail.com>
2020-03-17 10:55:20 +0100

    Update url and views
    viewにAPIを使って質問のリストから最新５つの項目を表示するようにindexビューを編集

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from .models import Question
 
 '''
 簡単なビュー作成。ビューを呼び出すためにはURLを対応づける必要がある -> URLconf
@@ -16,7 +17,10 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # 最新５つの質問をレスポンスする
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
