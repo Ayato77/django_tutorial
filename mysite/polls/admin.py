a8f735f2dab4e70b7197e618ab4fdb5b78672f42
Otoya <otoya.nakakaze@gmail.com>
2020-03-20 13:30:18 +0100

    Update admin.py
    Questionを新たに追加したり、編集したりするときに、Choiceの編集も同時できるように仕様変更。

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 # Register your models here.
 # Pollsアプリをadminページ上で編集できるようにする
-admin.site.register(Question)
+# admin.site.register(Question)のみだとデフォルトのadminページになるので、変更してみる
+# admin.StackInlineだと入力フォームの表示が縦に重なる。TabularInlineだと横に並ぶ
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+'''
+adminのオプションを変更したいときは、モデルごとにadminクラスを作成して、admin.site.register()の２番目の引数に渡す
+'''
+admin.site.register(Question, QuestionAdmin)
+
+# Choiceの管理ページを表示する
+# admin.site.register(Choice)
+# -> Questionモデルを追加するときにChoiceも追加したいので書き換える
